5b6d22bbadaa2235d125ed2595846e8dfaa35e2f
hodadis <hodadisbirhan80@gmail.com>
2024-03-17 03:51:40 +0300

    for add seo at app.vue file

--- app.vue
+++ app.vue
@@ -3,3 +3,13 @@
     <NuxtLayout> <NuxtPage /></NuxtLayout>
   </div>
 </template>
+<script setup>
+useSeoMeta({
+  title: "Hodadis",
+  ogTitle: "Hodadis",
+  description:
+    "Personal Portifolio,Hodadis Birhan,Amazing portfolio,Best portfolio,Best Personal Website,Hodadis Birhan.",
+  ogDescription: "This is my amazing site, let me tell you all about it.",
+  ogImage: "/hod.png",
+});
+</script>
